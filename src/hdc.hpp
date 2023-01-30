79a8ca308bcc12717f4746e26303243bc7377fad
Caio Vieira <caioxyz@hotmail.com>
2023-01-30 12:41:22 -0300

    hdc: Add new API
    The new API is built to support vectors int, float, double, and binary
    vectors.

--- src/hdc.hpp
+++ src/hdc.hpp
@@ -5,6 +5,10 @@
 #include <vector>
 
 #include "types.hpp"
+#include "AssociativeMemory.hpp"
+#include "ContinuousItemMemory.hpp"
+#include "ItemMemory.hpp"
+#include "Vector.hpp"
 
 namespace hdc {
     using vec_t = std::uint32_t;
@@ -39,6 +43,52 @@ namespace hdc {
     HDV maj(const HDV &op1, const HDV &op2, const HDV &op3);
     HDV maj(const std::vector<hdc::HDV> &v);
     HDV mul(const std::vector<hdc::HDV> &v);
+
+    // Vector data types
+    using int32_t = Vector<int32_t>;
+    using float_t = Vector<float>;
+    using double_t = Vector<double>;
+    using bin_t = Vector<bin_vec_t>;
+
+    template<typename T>
+    float dist(const T& v1, const T& v2) {
+        return v1.dist(v2);
+    }
+
+    template<typename T>
+    T add(const T& v1, const T& v2, const T& v3) {
+        T res(v1);
+        res.add(v2, v3);
+        return res;
+    }
+
+    template<typename T>
+    T add(const std::vector<T>& vectors) {
+        return T::add(vectors);
+    }
+
+    template<typename T>
+    T mul(const T& v1, const T& v2) {
+        T res(v1);
+        res.mul(v2);
+        return res;
+    }
+
+    template<typename T>
+    T mul(const std::vector<T>& vectors) {
+        T res(vectors[0]);
+        for (auto i = 1; i < vectors.size(); i++) {
+            res.mul(vectors[i]);
+        }
+        return res;
+    }
+
+    template<typename T>
+    T p(const T& v1, std::uint32_t times) {
+        T res(v1);
+        res.p(times);
+        return res;
+    }
 }
 
 std::ostream& operator<<(std::ostream &os, const hdc::HDV &v);
